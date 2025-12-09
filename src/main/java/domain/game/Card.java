d34045212012cab78796e01e3e544037005a1011
Joanna Soltys <joannasoltys2026@u.northwestern.edu>
2025-12-08 21:38:00 -0600

    refactored Card

--- src/main/java/domain/game/Card.java
+++ src/main/java/domain/game/Card.java
@@ -1,15 +1,14 @@
 package domain.game;
 
 public class Card {
-	private CardType cardType;
+	private final CardType cardType;
 
 	public Card(domain.game.CardType cardType) {
 		this.cardType = cardType;
 	}
 
-	public domain.game.CardType getCardType() {
-		return cardType;
+	public boolean isCardType(CardType otherCardType) {
+		return this.cardType == otherCardType;
 	}
-
 }
 
